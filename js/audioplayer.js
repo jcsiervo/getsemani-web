6ac3ee119798f88c79139fe05b0766fed20670b5
Julio Arias <jcsiervo8701parado@yahoo.com>
2016-12-30 17:27:46 -0500

    player changes predicaciones

---
+++ js/audioplayer.js
@@ -0,0 +1,106 @@
+// html5media enables <video> and <audio> tags in all major browsers
+// External File: http://api.html5media.info/1.1.8/html5media.min.js
+
+
+// Add user agent as an attribute on the <html> tag...
+// Inspiration: http://css-tricks.com/ie-10-specific-styles/
+var b = document.documentElement;
+b.setAttribute('data-useragent', navigator.userAgent);
+b.setAttribute('data-platform', navigator.platform);
+
+
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+jQuery(function ($) {
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false,
+            mediaPath = 'media/Predicaciones/',
+            extension = '',
+            tracks = [ {
+                "track": 1,
+                "name": "Retiro de Matrimonios - Metodos Diferentes - Parte 1",
+                "length": "01:11:35",
+                "file": "Matrimonios-Metodos_Diferentes_1"
+            }, {
+                "track": 2,
+                "name": "Retiro de Matrimonios - Metodos Diferentes - Parte 2",
+                "length": "46:12",
+                "file": "Matrimonios-Metodos_Diferentes_2"
+            }
+            ],
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').bind('play', function () {
+                playing = true;
+                npAction.text('Now Playing...');
+            }).bind('pause', function () {
+                playing = false;
+                npAction.text('Paused...');
+            }).bind('ended', function () {
+                npAction.text('Paused...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').click(function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').click(function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').click(function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+            if(audio.canPlayType('audio/ogg')) {
+                extension = '.ogg';
+            }
+            if(audio.canPlayType('audio/mpeg')) {
+                extension = '.mp3';
+            }
+            loadTrack(index);
+    }
+});
